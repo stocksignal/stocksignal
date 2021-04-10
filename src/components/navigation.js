18e08d2a362b3e237e3583bb7aa108657a459ff3
Stock Signal <82221987+stocksignal@users.noreply.github.com>
2021-04-10 20:31:15 +0530

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+    <a href="https://stocksignal.github.io">Home</a>
     <ThemeChanger/>
   </nav>
   
